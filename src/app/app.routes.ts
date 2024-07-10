9ec8d489a2d3676e3c981380c48600353eb53a07
raydevcp20 <rayanecristina.19@gmail.com>
2024-07-10 08:21:41 -0300

    feat: preparação de código para aula 03, 10/07

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { StudentListComponent } from './pages/student-list/student-list.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'login', pathMatch: 'full' },
     { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent},
+    { path: 'students', component: StudentListComponent}
 ];
