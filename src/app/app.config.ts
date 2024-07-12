1ab3c6c904f2f5788361276dc3332d7be4b9b8da
raydevcp20 <rayanecristina.19@gmail.com>
2024-07-11 23:44:10 -0300

    feat: Código visto na aula 04, dia 11/07

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,11 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), provideAnimationsAsync()]
 };
+
+//O método provideAnimationsAsync() é um método de fábrica que retorna um 
+//objeto de configuração para o módulo de animações usado pelo angular material.
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,16 +1,19 @@
 import { Component } from '@angular/core';
-import { InputComponent } from '../../shared/components/input/input.component';
-import { ButtonComponent } from '../../shared/components/button/button.component';
 import { FormsModule } from '@angular/forms';
-
+import {MatInputModule} from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatCardModule} from '@angular/material/card';
 @Component({
   selector: 'app-login',
   standalone: true, //Componente é dado como standalone
   //e com isso é necessário importar qualquer dependência que ele necessite
-  imports: [//Como o componente de input e botão é usado, então é necessário importar
-    InputComponent, 
-    ButtonComponent, 
-    FormsModule], 
+  imports: [
+    FormsModule, 
+    //Importações do Angular Material por componente
+    MatFormFieldModule,
+    MatInputModule, 
+    MatCardModule
+  ], 
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
