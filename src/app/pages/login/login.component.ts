86e2a9c016e8572fbcce7cff6a5b540d46e68b27
raydevcp20 <rayanecristina.19@gmail.com>
2024-07-12 21:56:58 -0300

    feat: Código visto na aula 05 revisão, dia 12/07

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,22 +1,31 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import {MatInputModule} from '@angular/material/input';
+import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import {MatCardModule} from '@angular/material/card';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialog } from '@angular/material/dialog';
+import { ModalComponent } from '../../shared/components/modal/modal.component';
+
 @Component({
   selector: 'app-login',
   standalone: true, //Componente é dado como standalone
   //e com isso é necessário importar qualquer dependência que ele necessite
   imports: [
-    FormsModule, 
+    FormsModule,
     //Importações do Angular Material por componente
     MatFormFieldModule,
-    MatInputModule, 
-    MatCardModule
-  ], 
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+  ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  constructor(private diolog: MatDialog) {}
 
+  openModal() {
+    this.diolog.open(ModalComponent);
+  }
 }
--- src/app/shared/components/modal/modal.component.ts
+++ src/app/shared/components/modal/modal.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import {
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogRef,
+  MatDialogTitle,
+} from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-modal',
   standalone: true, //Componente é dado como standalone
-  imports: [],
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule,
+    MatDialogTitle,
+    MatDialogContent,
+    MatDialogActions,
+    MatDialogClose,
+  ],
   templateUrl: './modal.component.html',
-  styleUrl: './modal.component.scss'
+  styleUrl: './modal.component.scss',
 })
 export class ModalComponent {
+  email: string = '';
+  readonly dialogRef = inject(MatDialogRef<ModalComponent>);
 
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
 }
